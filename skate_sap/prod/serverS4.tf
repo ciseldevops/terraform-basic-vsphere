195c1406ce00890b82b66b1d0dca27e83bd0fcee
MEYLSI <48313900+MEYLSI@users.noreply.github.com>
2019-10-08 15:12:37 +0200

    v1

--- skate_sap/prod/serverS4.tf
+++ skate_sap/prod/serverS4.tf
@@ -1,16 +1,32 @@
-module "virtual_machines_s4" {
-  source               = "../modules/vsphere_vm"
-  datacenter           = "${var.datacenter}"
-  datastore            = "${var.datastore}"
-  network              = "${var.network}"
-  resource_pool        = "${var.resource_pool}"
-  
-  ipv4_address_start   = "${var.s4_ipv4_address_start}"
-  ipv4_gateway         = "${var.s4_ipv4_gateway}"
-  ipv4_network_address = "${var.s4_ipv4_network_address}"
-  
-  template_name        = "${var.s4_template_name}"
-  vm_name_prefix       = "${var.s4_vm_name_prefix}"
-  vm_count             = "1"
+#module "virtual_machines" {
+#  source               = "../modules/vsphere_vm"
+#  datacenter           = "${var.datacenter}"
+#  datastore            = "${var.datastore}"
+#  network              = "${var.network}"
+#  resource_pool        = "${var.resource_pool}"
+#  
+#  ipv4_address_start   = "${var.s4_ipv4_address_start}"
+#  ipv4_gateway         = "${var.s4_ipv4_gateway}"
+#  ipv4_network_address = "${var.s4_ipv4_network_address}"
+#  
+#  template_name        = "${var.s4_template_name}"
+#  vm_name_prefix       = "${var.s4_vm_name_prefix}"
+#  vm_count             = "1"
+#
+#}
 
-}
+
+
+ module "virtual_machines" {
+   source                     = "vancluever/terraform-vsphere-virtual-machine"
+   version                    = "1.0.0"
+   datacenter                 = "${var.datacenter}"
+   datastore                  = "${var.datastore}"
+   disk_size                  = "20"
+   guest_id                   = "otherLinuxGuest"
+   memory                     = "2048"
+   network                    = "${var.network}"
+   resource_pool              = "${var.resource_pool}"
+   vm_count                   = "1"
+   vm_name_prefix             = "srv"
+ }
