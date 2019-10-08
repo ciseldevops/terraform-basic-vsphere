d1e94ca7341a925b338d740a1da442554ab12b2d
MEYLSI <48313900+MEYLSI@users.noreply.github.com>
2019-10-08 14:10:00 +0200

    asdf

--- skate_sap/prod/serverS4.tf
+++ skate_sap/prod/serverS4.tf
@@ -1,16 +1,16 @@
-module "virtual_machines_s4" {
-  source               = "../modules/vsphere_vm"
-  datacenter           = "${var.datacenter}"
-  datastore            = "${var.datastore}"
-  network              = "${var.network}"
-  resource_pool        = "${var.resource_pool}"
-  
-  ipv4_address_start   = "${var.s4_ipv4_address_start}"
-  ipv4_gateway         = "${var.s4_ipv4_gateway}"
-  ipv4_network_address = "${var.s4_ipv4_network_address}"
-  
-  template_name        = "${var.s4_template_name}"
-  vm_name_prefix       = "${var.s4_vm_name_prefix}"
-  vm_count             = "1"
-
-}
+#module "virtual_machines_s4" {
+#  source               = "../modules/vsphere_vm"
+#  datacenter           = "${var.datacenter}"
+#  datastore            = "${var.datastore}"
+#  network              = "${var.network}"
+#  resource_pool        = "${var.resource_pool}"
+#  
+#  ipv4_address_start   = "${var.s4_ipv4_address_start}"
+#  ipv4_gateway         = "${var.s4_ipv4_gateway}"
+#  ipv4_network_address = "${var.s4_ipv4_network_address}"
+#  
+#  template_name        = "${var.s4_template_name}"
+#  vm_name_prefix       = "${var.s4_vm_name_prefix}"
+#  vm_count             = "0"
+#
+#}
