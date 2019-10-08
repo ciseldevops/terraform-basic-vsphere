7a6da68f7b0ff921e30b4a7069b09d5606a73917
MEYLSI <48313900+MEYLSI@users.noreply.github.com>
2019-10-08 15:06:45 +0200

    v1

--- skate_sap/prod/serverS4.tf
+++ skate_sap/prod/serverS4.tf
@@ -1,74 +1,16 @@
-############################# GLOBAL #############################
+module "virtual_machines_s4" {
+  source               = "../modules/vsphere_vm"
+  datacenter           = "${var.datacenter}"
+  datastore            = "${var.datastore}"
+  network              = "${var.network}"
+  resource_pool        = "${var.resource_pool}"
+  
+  ipv4_address_start   = "${var.s4_ipv4_address_start}"
+  ipv4_gateway         = "${var.s4_ipv4_gateway}"
+  ipv4_network_address = "${var.s4_ipv4_network_address}"
+  
+  template_name        = "${var.s4_template_name}"
+  vm_name_prefix       = "${var.s4_vm_name_prefix}"
+  vm_count             = "1"
 
-#--- CREDENTIALS
-variable "vsphere_user" {
-  description = "vSphere LAB User"
-  default = "administrator@vsphere.local"
-}
-variable "vsphere_password" {
-  description = "vSphere LAB User"
-  #PASSWORD TO ENTER EVERY TIME
-}
-variable "vsphere_server" {
-  description = "vSphere LAB Server"
-  default = "10.210.8.65"
-}
-
-#--- DATACENTER 
-variable "datacenter" {
-  description = "Datacenter"
-  default = "MATRAN"
-}
-#--- VLAN 
-variable "network"{
-  description = "LAB-1_VLAN2247"
-  default = "LAB-1_VLAN2247"
-}
-#--- DATASTORE 
-variable "datastore"{
-  description = "Local datastore1"
-  default = "datastore1"
-}
-#--- RESSOURCE POOL
-variable "resource_pool"{
-  description = "resource pool"
-  default = "LAB-1/Resources"
-}
-
-############################# PROD #############################
-
-############################# S4
-#--- TEMPLATE 
-variable "s4_template_name"{
-  description = "Template W2016"
-  default = "WIN2016V2"
-}
-#--- FOLDER 
-variable "s4_folder_terraform"{
-  description = "Dossier VM Terraform"
-  default = "TestSAP"
-}
-#--- VMNAME 
-variable "s4_vm_name"{
-  description = "Nom de la VM 1"
-  default = "VMHANA_sme"
-}
-variable "s4_vm_name_prefix"{
-  description = "Nom de la VM 1"
-  default = "test_"
-}
-
-variable "s4_ipv4_network_address" {
-  description = "The network address to assign during customization of cloned virtual machines, in A.B.C.D/XX format. Leave at the default value for DHCP."
-  default     = "10.210.8.0/24"
-}
-
-variable "s4_ipv4_address_start" {
-  description = "The IP address to start assigning virtual machines at, relative to the network address and mask. Example: for two virtual machines in 10.0.0.0/24, a value of 10 here would give the IP addresses 10.0.0.10 and 10.0.0.11. For 10.0.0.128/25, a value of 10 would give 10.0.0.138 and 10.0.0.139."
-  default     = "22"
-}
-
-variable "s4_ipv4_gateway" {
-  description = "The default IPv4 gateway for the virtual machines. Leave blank for DHCP."
-  default     = "10.210.8.1"
 }
--- skate_sap/prod/serverS4_var.tf
+++ skate_sap/prod/serverS4_var.tf
@@ -1,3 +1,43 @@
+############################# GLOBAL #############################
+
+#--- CREDENTIALS
+variable "vsphere_user" {
+  description = "vSphere LAB User"
+  default = "administrator@vsphere.local"
+}
+variable "vsphere_password" {
+  description = "vSphere LAB User"
+  #PASSWORD TO ENTER EVERY TIME
+}
+variable "vsphere_server" {
+  description = "vSphere LAB Server"
+  default = "10.210.8.65"
+}
+
+#--- DATACENTER 
+variable "datacenter" {
+  description = "Datacenter"
+  default = "MATRAN"
+}
+#--- VLAN 
+variable "network"{
+  description = "LAB-1_VLAN2247"
+  default = "LAB-1_VLAN2247"
+}
+#--- DATASTORE 
+variable "datastore"{
+  description = "Local datastore1"
+  default = "datastore1"
+}
+#--- RESSOURCE POOL
+variable "resource_pool"{
+  description = "resource pool"
+  default = "LAB-1/Resources"
+}
+
+############################# PROD #############################
+
+############################# S4
 #--- TEMPLATE 
 variable "s4_template_name"{
   description = "Template W2016"
