98293b45014b4d5860c8098a0f9a51aff0c84a08
MEYLSI <48313900+MEYLSI@users.noreply.github.com>
2019-10-10 16:23:09 +0200

    v1.2

--- skate_sap/prod/servers.tf
+++ skate_sap/prod/servers.tf
@@ -31,6 +31,7 @@ module "server-windowsvm-withdatadisk" {
   vmname        = "server-windowssme"
   vmrp          = "LAB-1/Resources"
   network_cards = ["LAB-1_VLAN2247"]
+  vmgateway = "10.210.8.1"
   ipv4 = {
     "LAB-1_VLAN2247" = ["10.210.8.26"] # To use DHCP create Empty list for each instance
   }
@@ -38,6 +39,6 @@ module "server-windowsvm-withdatadisk" {
   datastore = "datastore1"
   run_once = [
           "netsh advfirewall firewall add rule name=\"ICMP Allow incoming V4 echo request\" protocol=icmpv4:8,any dir=in action=allow",
-          "Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server'-name \"fDenyTSConnections\" -Value 0",
+          "reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server\" /v fDenyTSConnections /t REG_DWORD /d 0 /f",
           "Enable-NetFirewallRule -DisplayGroup \"Remote Desktop\""]
 }
